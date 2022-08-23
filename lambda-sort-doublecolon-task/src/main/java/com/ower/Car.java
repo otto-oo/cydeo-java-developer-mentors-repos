419c2bbd12fa816cbeced8a2d9c29f2cf579463a
owermaiden <ower.maiden@cybertekschool.com>
2022-08-23 16:37:30 +0200

    Comparator

---
+++ lambda-sort-doublecolon-task/src/main/java/com/ower/Car.java
@@ -0,0 +1,35 @@
+package com.ower;
+
+public class Car  {
+    private String model;
+    private Integer year;
+
+    public Car(String model, Integer year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+}
---
+++ lambda-sort-doublecolon-task/src/main/java/com/ower/MiniCar.java
@@ -0,0 +1,8 @@
+package com.ower;
+
+public class MiniCar extends Car{
+
+    public MiniCar(String model, Integer year) {
+        super(model, year);
+    }
+}
