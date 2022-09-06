0563666e6f9595b88e4c846f5b50144aede80b79
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2022-09-06 19:08:51 +0200

    solid principles' module is added

---
+++ solid_principles/src/main/java/dependency_inversion/solution/Driver.java
@@ -0,0 +1,18 @@
+package dependency_inversion.solution;
+
+/**
+ * Created by bsferreira on 17-04-2016.
+ */
+public class Driver {
+    private final Vehicle vehicle;
+
+    public Driver(final Vehicle vehicle){
+        this.vehicle = vehicle;
+    }
+
+    public void increaseSpeed(){
+        vehicle.accelerate();
+    }
+}
+
+
---
+++ solid_principles/src/main/java/open_closed/solution/DrivingMode.java
@@ -0,0 +1,10 @@
+package open_closed.solution;
+
+/**
+ * Created by bsferreira on 16-04-2016.
+ */
+public interface DrivingMode {
+
+    int getPower();
+    int getSuspensionHeight();
+}
