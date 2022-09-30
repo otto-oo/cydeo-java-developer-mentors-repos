bdc0e64bd142c6f8885d5f3ace1edc1466e4546d
owermaiden <ower.maiden@cybertekschool.com>
2022-10-01 00:15:35 +0200

    Algo Solutions week-24

--- weeks-algos/src/main/java/week_24/ArrayOfProducts.java
+++ weeks-algos/src/main/java/week_24/ArrayOfProducts.java
@@ -1,26 +1,72 @@
 package week_24;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class ArrayOfProducts {
 
     public static void main(String[] args) {
-        int[] givenArray = {5,1,4,2};
-        System.out.println(Arrays.toString(productsOfOthers(givenArray)));
+        int[] givenArray = {1,1};
+        System.out.println(Arrays.toString(productExceptSelf(givenArray)));
     }
 
-    public static int[] productsOfOthers(int[] givenArray) {
-        var resultArray = new ArrayList<Integer>();
-
-        int pointer = 0;
-
-        while (pointer < givenArray.length) {
-            int finalPointer = pointer;
-            int carry = Arrays.stream(givenArray).filter(n -> n != givenArray[finalPointer]).reduce(1, (a, b) -> a * b);
-            resultArray.add(carry);
-            pointer++;
+    public static int[] productsOfOthers(int[] nums) {
+        var resultArray = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int finalI = i;
+            AtomicInteger temp = new AtomicInteger(1);
+         var list=   Arrays.stream(nums).boxed()
+                .filter(num -> num != nums[finalI])
+                .map(n -> temp.getAndUpdate(v -> v * n)).toList();
+            resultArray[i] = temp.get();
         }
-        return resultArray.stream().mapToInt(i -> i).toArray();
+        return resultArray;
+    }
+    public static int[] productExceptSelf(int[] nums) {
+
+        // The length of the input array
+        int length = nums.length;
+
+        // The left and right arrays as described in the algorithm
+        int[] L = new int[length];
+        int[] R = new int[length];
+
+        // Final answer array to be returned
+        int[] answer = new int[length];
+
+        // L[i] contains the product of all the elements to the left
+        // Note: for the element at index '0', there are no elements to the left,
+        // so L[0] would be 1
+        L[0] = 1;
+        for (int i = 1; i < length; i++) {
+
+            // L[i - 1] already contains the product of elements to the left of 'i - 1'
+            // Simply multiplying it with nums[i - 1] would give the product of all
+            // elements to the left of index 'i'
+            L[i] = nums[i - 1] * L[i - 1];
+        }
+
+        // R[i] contains the product of all the elements to the right
+        // Note: for the element at index 'length - 1', there are no elements to the right,
+        // so the R[length - 1] would be 1
+        R[length - 1] = 1;
+        for (int i = length - 2; i >= 0; i--) {
+
+            // R[i + 1] already contains the product of elements to the right of 'i + 1'
+            // Simply multiplying it with nums[i + 1] would give the product of all
+            // elements to the right of index 'i'
+            R[i] = nums[i + 1] * R[i + 1];
+        }
+
+        // Constructing the answer array
+        for (int i = 0; i < length; i++) {
+            // For the first element, R[i] would be product except self
+            // For the last element of the array, product except self would be L[i]
+            // Else, multiple product of all elements to the left and to the right
+            answer[i] = L[i] * R[i];
+        }
+
+        return answer;
     }
 }
