8405b8b3721227c2e4377764dda8edd0b4cff261
aymehmet <aymehmet1976@gmail.com>
2022-12-14 17:02:32 +0300

    Week-34 is modified..

--- weeks-algos/src/main/java/week_30/LongestValidParanteses.java
+++ weeks-algos/src/main/java/week_30/LongestValidParanteses.java
@@ -7,7 +7,7 @@ public class LongestValidParanteses {
 
     public static void main(String[] args) {
 
-        System.out.println(longestValidParentheses("(())...)"));
+        System.out.println(longestValidParentheses("()(())"));
 
     }
 
