d1dcade5ff6deefeb81051cccd9785ab2fb1cff9
Sumeyse <sumeyse@gmail.com>
2023-03-16 14:40:31 +0300

    3 intw Qs added

---
+++ z2h-interviewSupport/src/main/java/Intw_DSA_Qs/StockProblem.java
@@ -0,0 +1,14 @@
+package Intw_DSA_Qs;
+
+public class StockProblem {
+/*
+    Given an array that represents the stock prices of consecutive months and analysis factor k
+    find how many periods where stocks prices are strictly increasing , the period should be of k length
+    Ex:
+    arr= [ 5,3,5,6,8] ,  k=3
+    answer :
+            2
+    arr=[1,2,3,4] , k=4
+    answer
+    1*/
+}
